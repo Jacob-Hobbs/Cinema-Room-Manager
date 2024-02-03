1042000bd99567bc43d74aa76f0a09410c2e3cd9
Jacob Hobbs <JacobRayHobbs@gmail.com>
2024-02-03 10:44:38 -0600

    First commit of 02/03
    Halfway done with part 3; now just need to get updated cinema printed

--- Cinema Room Manager/task/src/cinema/Layout.java
+++ Cinema Room Manager/task/src/cinema/Layout.java
@@ -4,34 +4,37 @@ public class Layout {
 
     private final String[][] cinemaArray;
 
-    public Layout() {
-        this.cinemaArray = new String[9][18];
+    public Layout(int rows, int seats) {
+        this.cinemaArray = new String[rows + 2][seats + seats + 2];
     }
 
     // Controls the execution order of Layout statements
-    public void start() {
-        initializeEmptyCinema();
-        printEmptyCinema();
+    public void start(int rows, int seats) {
+        initializeEmptyCinema(rows, seats);
+        printCinema(rows, seats);
     }
 
     // Initializes empty cinema seats within 2D Array
-    public void initializeEmptyCinema() {
-        for (int i = 1; i < 9; i++) {
-            for (int j = 1; j < 18; j++) {
+    public void initializeEmptyCinema(int rows, int seats) {
+        
+        //this.cinemaArray = new String[rows + 2][seats + seats + 2];
+        
+        for (int i = 1; i < (rows + 2); i++) {
+            for (int j = 1; j < (seats + seats + 2); j++) {
                 if (i == 1) {
                     if ((j < 3) || (j % 2 == 0)) {
-                        cinemaArray[i][j] = " ";
+                        this.cinemaArray[i][j] = " ";
                     } else {
-                        cinemaArray[i][j] = String.valueOf((j - 1) / 2);
+                        this.cinemaArray[i][j] = String.valueOf((j - 1) / 2);
                     }
                 } else {
                     if (j == 1) {
-                        cinemaArray[i][j] = String.valueOf(i - 1);
+                        this.cinemaArray[i][j] = String.valueOf(i - 1);
                     } else {
                         if (j % 2 == 0) {
-                            cinemaArray[i][j] = " ";
+                            this.cinemaArray[i][j] = " ";
                         } else {
-                            cinemaArray[i][j] = "S";
+                            this.cinemaArray[i][j] = "S";
                         }
                     }
                 }
@@ -40,13 +43,22 @@ public class Layout {
     }
 
     // Prints empty cinema seats as stored in 2D Array
-    public void printEmptyCinema() {
+    public void printCinema(int rows, int seats) {
         System.out.println("Cinema:");
-        for (int i = 1; i < 9; i++) {
-            for (int j = 1; j < 18; j++) {
-                System.out.print(cinemaArray[i][j]);
+        for (int i = 1; i < (rows + 2); i++) {
+            for (int j = 1; j < (seats + seats + 2); j++) {
+                System.out.print(this.cinemaArray[i][j]);
             }
             System.out.println();
         }
+    }
+
+
+    public void occupySeat(int pickedRow, int pickedSeat, int rows, int seats) {
+        this.cinemaArray[pickedRow + 1][(pickedSeat + pickedSeat) + 1] = "B";
+    }
+
+    public String[][] getCinemaArray() {
+        return this.cinemaArray;
     }
 }
--- Cinema Room Manager/task/src/cinema/ProfitCalculator.java
+++ Cinema Room Manager/task/src/cinema/ProfitCalculator.java
@@ -12,7 +12,52 @@ public class ProfitCalculator {
 
     // Controls the execution order of ProfitCalculator statements
     public void start() {
-        numOfRowsAndSeats();
+        int numOfRows = numOfRows();
+        int numOfSeats = numOfSeats();
+        System.out.println();
+        Layout layout = new Layout(numOfRows, numOfSeats);
+        layout.start(numOfRows, numOfSeats);
+        System.out.println();
+        purchaseTicket(numOfRows, numOfSeats);
+    }
+
+    // Intakes row and seat number and outputs ticket cost
+    public void purchaseTicket(int numOfRows, int numOfSeats) {
+        System.out.println("Enter a row number:");
+        int rowNum = Integer.valueOf(scanner.nextLine());
+        System.out.println("Enter a seat number in that row:");
+        int seatNum = Integer.valueOf(scanner.nextLine());
+        System.out.println();
+        System.out.println("Ticket price: $" + calculateSeatCost(rowNum, numOfRows, numOfSeats));
+        System.out.println();
+        // FIXME : Creates new array instead of updating existing one.
+        Layout layout = new Layout(numOfRows, numOfSeats);
+        layout.occupySeat(rowNum, seatNum, numOfRows, numOfSeats);
+        layout.printCinema(numOfRows, numOfSeats);
+    }
+
+    // calculates and returns seat cost based on location in cinema
+    public int calculateSeatCost(int rowNum, int numOfRows, int numOfSeats) {
+        int totalSeats = numOfRows * numOfSeats;
+        int frontHalfRows = (numOfRows / 2);
+        //int backHalfRows = (numOfRows - frontHalfRows);
+        if (totalSeats <= 60 || rowNum <= frontHalfRows) {
+            return 10;
+        } else {
+            return 8;
+        }
+    }
+
+    // Gathers and returns the number of rows in the cinema
+    public int numOfRows() {
+        System.out.println("Enter the number of rows:");
+        return Integer.valueOf(scanner.nextLine());
+    }
+
+    // Gathers and returns the number of seats in the cinema
+    public int numOfSeats() {
+        System.out.println("Enter the number of seats in each row:");
+        return Integer.valueOf(scanner.nextLine());
     }
 
     // Accepts input to declare number of rows and seats in cinema
@@ -21,6 +66,10 @@ public class ProfitCalculator {
         int numOfRows = Integer.valueOf(scanner.nextLine());
         System.out.println("Enter the number of seats in each row:");
         int numOfSeats = Integer.valueOf(scanner.nextLine());
+        totalIncome(numOfRows, numOfSeats);
+    }
+
+    public void totalIncome(int numOfRows, int numOfSeats) {
         System.out.println("Total income:");
         System.out.println("$" + calculateTotalProfit(numOfRows, numOfSeats));
     }
